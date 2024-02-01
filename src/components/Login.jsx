024be8b203a9b9f515f8539b5608177976b6e589
Bhanu Vamshi Anumula <bhanuvamshi0211@gmail.com>
2024-02-01 13:40:06 +0530

    new logo added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = (props) => {
         {props.user && <Navigate to="/home" />}
         <Nav>
           <a href="/">
-            <img src="/images/login-logo.svg" alt="" />
+            <img src="/images/linkedin-mini.png" alt="" />
           </a>
           <div>
             <Join>Join now</Join>
@@ -67,6 +67,9 @@ const Join = styled.a`
   border-radius: 50px;
   color: rgba(0, 0, 0, 0.6);
   margin-right: 12px;
+  @media (max-width: 435px) {
+    display: flex;
+  }
   &:hover {
     background-color: rgba(0, 0, 0, 0.08);
     color: rgba(0, 0, 0, 0.9);
